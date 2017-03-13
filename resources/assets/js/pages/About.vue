aad7f44b4278a542398e3d5908a2eae9cdec4c1d
Harry <harry_mercury@hotmail.co.uk>
2017-03-13 22:22:12 +0000

    push to test deploy script
    wonder if this will break forge

--- resources/assets/js/pages/About.vue
+++ resources/assets/js/pages/About.vue
@@ -6,7 +6,7 @@
                     <div class="panel-heading">Example Component</div>
 
                     <div class="panel-body">
-                        About
+                        About page goes here
                     </div>
                 </div>
             </div>
