94854e6b1e6fae342c87cfb26c254b11c86269db
Harry <harry_mercury@hotmail.co.uk>
2017-03-15 11:43:26 +0000

    update algorithm slightly

--- app/Console/Commands/UpdateFighters.php
+++ app/Console/Commands/UpdateFighters.php
@@ -65,12 +65,14 @@ class UpdateFighters extends Command
         {
             //fights older than 3 years carry no weight
             $cumrp = ['wsw' => 0,'flw' => 0,'bw' => 0,'wbw' => 0,'fw' => 0,'wfw' => 0,'lw' => 0,'ww' => 0,'mw' => 0,'lhw' => 0,'hw' => 0];
+            $cumcount = ['wsw' => 0,'flw' => 0,'bw' => 0,'wbw' => 0,'fw' => 0,'wfw' => 0,'lw' => 0,'ww' => 0,'mw' => 0,'lhw' => 0,'hw' => 0];
             //2) Fights between 2 and 3 years ago are worth 1/3 points
             $snapshots_3 = DB::table('snapshots')->whereBetween('date',[$threeYearsAgoTomorrow, $twoYearsAgoToday])->where('fighter_id','=',$fighter->id)->get();
                 //where('date','<=',$today->subYear(3)->format('Y-m-d'))->where('date','>',$today->subYear(2)->format('Y-m-d'))->where('fighter_id','=',$fighter->id)->get()
             foreach($snapshots_3 as $snap3)
             {
                 $cumrp[$snap3->weightclass] += $snap3->rp_change*(1/3);
+                $cumcount[$snap3->weightclass] ++;
             }
             //2) Fights last year are worth 2/3 points
             $snapshots_2 = DB::table('snapshots')->whereBetween('date',[$twoYearsAgoTomorrow,$aYearAgoToday])->where('fighter_id','=',$fighter->id)->get();
@@ -78,16 +80,36 @@ class UpdateFighters extends Command
             {
 
                 $cumrp[$snap2->weightclass] += $snap2->rp_change*(2/3);
+                $cumcount[$snap2->weightclass] ++;
             }
             //3) Fights this year are worth full points
             $snapshots_1 = DB::table('snapshots')->whereBetween('date',[$aYearAgoTomorrow,$today])->where('fighter_id','=',$fighter->id)->get();
             foreach($snapshots_1 as $snap1)
             {
                 $cumrp[$snap1->weightclass] += $snap1->rp_change;
+                $cumcount[$snap1->weightclass] ++;
             }
             $bar->advance();
             foreach($cumrp as $classy => $classyval)
             {
+                if($cumcount[$classy] < 2)
+                {
+                    $classyval = $classyval * 2;
+                }
+                elseif($cumcount[$classy] <3)
+                {
+                    $classyval = $classyval * 1.75;
+                }
+                elseif($cumcount[$classy] <4)
+                {
+                    $classyval = $classyval * 1.5;
+                }
+                elseif($cumcount[$classy] < 5)
+                {
+                    $classyval = $classyval * 1.25;
+                }
+
+
                 $fighter->$classy = $classyval;
             }
             $fighter->save();
